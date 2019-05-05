77e4fbd9138a9bfba6c75853004a59d235bcda58
Marina <marina.2000.marina@inbox.ru>
2019-05-05 23:17:39 +0400

    commit

--- FileSortProject/main.cpp
+++ FileSortProject/main.cpp
@@ -1,25 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
- * Author: kamabb
+ * Author: Marina
  *
  * Created on 5 мая 2019 г., 22:09
  */
 
-#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-
+int main() {
+    setlocale(LC_ALL, "ru");
+    
+    
     return 0;
 }
 
